a25514d030c6a35e94a55a00df8d3be6d455d2ab
Neil Kimmett <neilkimmett@gmail.com>
2014-06-23 17:36:08 +0100

    Start to make custom subclass for PostCell

---
+++ ProductHunt/PostCell.swift
@@ -0,0 +1,16 @@
+//
+//  PostCell.swift
+//  ProductHunt
+//
+//  Created by Neil Kimmett on 22/06/2014.
+//  Copyright (c) 2014 Neil Kimmett. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet var avatarView : UIImageView
+    @IBOutlet var titleLabel : UILabel
+    @IBOutlet var taglineLabel : UILabel
+    @IBOutlet var commentCountLabel : UILabel
+}
